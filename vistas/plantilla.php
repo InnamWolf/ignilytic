e6d712ad21163f29ab9bd8f51895e8a60cead7e2
Manuel Ramirez <m.ramirez@ignilytic.com>
2021-04-27 23:40:38 -0500

    Configuracion de SASS

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -14,6 +14,7 @@
   Librerias CSS
   =================================================-->
   <link rel="stylesheet" href="vistas/css/normalize.css"/>
+  <link rel="stylesheet" href="vistas/css/main.css">
   <!--===============================================
   Librerias JS
   =================================================-->
@@ -21,6 +22,6 @@
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-QLY0H0KFYK"></script>
 </head>
 <body>
-  hola
+  
 </body>
 </html>
