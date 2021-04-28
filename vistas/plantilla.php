649ad42029a927d90b052c8578237077d572ab23
Manuel Ramirez <m.ramirez@ignilytic.com>
2021-04-27 23:30:35 -0500

    Integracion de normalize

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,1 +1,26 @@
-hola
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <meta name="description" content="Diseño de Paginas Web, Desarrollo de Sistemas Web a la medida, Exito para tu Negocio">
+  <meta name="keyword" content="Web, Mexico, Dinamicas,  Página, Creativas, Diseño, Sistemas, Confiables, Economicas, Estables">
+  <meta name="author" content="http://www.ignilytic.com/">
+  <meta name="facebook-domain-verification" content="uc0ql1adbveoomnv2hhigtjnt4rnfd" />
+  <meta name="robots" content="inicio" />
+  <title>Ignilytic - Páginas Web</title>
+  <!--===============================================
+  Librerias CSS
+  =================================================-->
+  <link rel="stylesheet" href="vistas/css/normalize.css"/>
+  <!--===============================================
+  Librerias JS
+  =================================================-->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QLY0H0KFYK"></script>
+</head>
+<body>
+  hola
+</body>
+</html>
