b5c57ccc144c8782e2aa246bf4c280dda44ca9dd
Vicente Manuel Ramirez Reyes <vicente.ramirez@becasbenitojuarez.gob.mx>
2021-04-30 13:56:59 -0500

    Se cambio el menu de tablet por el de movil

--- vistas/js/main.js
+++ vistas/js/main.js
@@ -6,8 +6,8 @@ const contenedor = document.querySelector('.contenedor-item');
 
 menu.addEventListener('click', () => {
   if (contenedor.classList.contains('activo')) {
-    contenedor.classList.remove('activo');
     contenedor.classList.remove('animate__animated', 'animate__fadeInDown');
+    contenedor.classList.remove('activo');
   } else {
     contenedor.classList.add('activo');
     contenedor.classList.add('animate__animated', 'animate__fadeInDown');
