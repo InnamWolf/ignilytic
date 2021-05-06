750a94d3588ad2b39396dd2e3a0e99040831a4a8
Manuel <m.ramirez@ignilytic.com>
2021-05-05 22:44:05 -0500

    Se creo footer para resposivo y pc

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -150,6 +150,35 @@
       </p>
     </div>
   </section>
+
+  <!--===============================================
+  Footer
+  =================================================-->
+  <footer>
+    <div class="footer-dentro">
+        <div class="logo">
+          <a href="#">
+            <img src="vistas/img/logo.svg" alt="logo">
+            <span>Ignilytic</span>
+          </a>
+        </div>
+        <div class="footer-menu">
+          <a href="#">Inicio</a>
+          <a href="#">Servicios</a>
+          <a href="#">Portafolio</a>
+          <a href="#">FAQ</a>
+          <a href="#">Politica provacidad</a>
+        </div>
+        <div class="footer-sociales">
+          <p class="copi">&copy; 2020 www.ignilytic.com</p>
+          <div class="sociales">
+            <a href="#"><i class="fab fa-facebook-square"></i></a>
+            <a href="#"><i class="fas fa-globe"></i></a>
+            <a href="#"><i class="far fa-envelope"></i></a>
+          </div>
+        </div>
+    </div>
+  </footer>
 <script src="vistas/js/main.js"></script>
 </body>
 </html>
